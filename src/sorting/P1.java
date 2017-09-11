aeca7c83117a9c162b9d5fc1ce803991ed8dd43f
alexisherrera <halexis@sas.upenn.edu>
2017-09-11 08:20:51 -0400

    intersection of 2 sorted arrs

--- src/sorting/P1.java
+++ src/sorting/P1.java
@@ -48,17 +48,41 @@ public class P1 {
         int indexL1 = 0;
         int indexL2 = 0;
 
-        //we will iterate through the
+        //we will iterate through the two arrays simultaneously using a while loop.
+        //if we find that both elements are the same value we will add to intersection. if not we
+        //will update indices until they are at a new value.
+        while (indexL1 < l1.length && indexL2 < l2.length) {
+            //check if they are the same value
+            if (l1[indexL1] == l2[indexL2] && ((intersection.size() > 0 &&
+                    intersection.get(intersection.size() - 1) !=  l1[indexL1]) || intersection.size() == 0)) {
+                intersection.add(l1[indexL1]);
 
+                //update indices
+                indexL1++;
+                indexL2++;
 
+                //continue is critical because then it will go to the other conditionals
+                continue;
+            }
 
+            //lets update the smaller value's index by 1
+            if (l1[indexL1] < l2[indexL2]) {
+                indexL1++;
+            }
+            else if (l1[indexL1] > l2[indexL2]) {
+                indexL2++;
+            }
+        }
+
+        return intersection;
     }
+
 
     public static void main(String[] args) {
         int[] l1 = { 4, 7, 19, 20, 600};
         int[] l2 = { 7, 7 , 600, 900};
 
-        System.out.println(intersection(l1, l2));
+        System.out.println(intersectionImprovedSpaceComplexity(l1, l2));
     }
 
 }
