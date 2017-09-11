2010c8b59c93dfaa3b195c46122d3ed95e43ffba
alexisherrera <halexis@sas.upenn.edu>
2017-09-11 18:05:54 -0400

    fixed merging interval sorting

--- src/sorting/P6.java
+++ src/sorting/P6.java
@@ -60,7 +60,58 @@ public class P6 {
 
     //this is the way that it is done in EPI
     public static ArrayList<int[]> mergingIntervalsEPI(ArrayList<int[]> intervals, int[] interval) {
+        //this is a 3 step algorithm in which we first consider items that do not intersect
+        //with the range provided in the interval. Here we simply add these intervals to the resulting array.
+        //in the second phase of the algorithm we "build" our union, by unioning all intervals that
+        //intersect with the interval. When nothing intersects anymore, we just place everything else at the
+        //end of the array
 
+        ArrayList<int[]> result = new ArrayList<>();
+        int index = 0;
+
+        //phase one in the algorithm
+        while(index < intervals.size()) {
+            //interval we are at has starts and ends before interval we are trying to introduce.
+            if (intervals.get(index)[1] < interval[0]) {
+                result.add(intervals.get(index));
+                index++;
+            }
+            else {
+                break;
+            }
+        }
+
+        //phase two in the algorithm find unions
+
+        while (index < intervals.size()) {
+            if (intervals.get(index)[0] <= interval[1])  {
+                interval = union(interval, intervals.get(index));
+                index++;
+            }
+            else {
+                break;
+            }
+        }
+        result.add(interval);
+
+        //phase three in the algorithm, finish adding intervals that do not intersect
+        while (index < intervals.size()) {
+            result.add(intervals.get(index));
+            index++;
+        }
+
+        return result;
+    }
+
+    //function to find the union of two intersecting arrays
+    public static int[] union(int[] i1, int[] i2) {
+        int[] result= new int[2];
+
+        //basically extending our boundaries to include the different ranges. This works by making the min the smaller
+        //of the two values and the max the larger of the two values
+        result[0] = Math.min(i1[0], i2[0]);
+        result[1] = Math.max(i1[1], i2[1]);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -78,7 +129,7 @@ public class P6 {
         System.out.println("");
         int[] interval = {0, 13};
 
-        ArrayList<int[]> updatedList = mergeIntervals(list, interval);
+        ArrayList<int[]> updatedList = mergingIntervalsEPI(list, interval);
 
         for (int[] i : updatedList) {
             System.out.print("[" + i[0] + "], [" + i[1] + "]    ");
