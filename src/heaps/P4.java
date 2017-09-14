7ebc62812b9fd3b52bc99def2e6992ab9577065a
alexisherrera <halexis@sas.upenn.edu>
2017-09-14 10:59:49 -0400

    k closest stars

---
+++ src/heaps/P4.java
@@ -0,0 +1,12 @@
+package heaps;
+
+/**
+ * Created by alexisherrera on 9/14/17.
+ */
+public class P4 {
+    //k closest stars problems
+
+    //since we are concerned with finding the k closest stars we can utlize a max heap to store
+    //k stars
+
+}
