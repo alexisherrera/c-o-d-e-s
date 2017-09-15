86e654ea58ae0057185ef74fd71c26cb3ceedaa6
alexisherrera <halexis@sas.upenn.edu>
2017-09-15 09:09:55 -0400

    started dp lowkey yikes

---
+++ src/dynamic_programming/P1.java
@@ -0,0 +1,8 @@
+package dynamic_programming;
+
+/**
+ * Created by alexisherrera on 9/15/17.
+ */
+public class P1 {
+    //count num of score combinations
+}
---
+++ src/heaps/P5.java
@@ -0,0 +1,12 @@
+package heaps;
+
+/**
+ * Created by alexisherrera on 9/15/17.
+ */
+public class P5 {
+    //compute the median of online data
+
+    //we are given numbers in a streaming fashion and are asked to compute the median on the spot after
+    //we process each number. One approach to doing this is to
+
+}
