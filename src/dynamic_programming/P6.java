9324dc2ccc135c5b4c4f358619619141452f8999
alexisherrera <halexis@sas.upenn.edu>
2017-09-25 09:14:18 -0400

    finished knapsack

--- src/dynamic_programming/P6.java
+++ src/dynamic_programming/P6.java
@@ -1,6 +1,7 @@
 package dynamic_programming;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,24 +18,40 @@ public class P6 {
         //for instance, iterate through the clocks selecting and not selecting a particular clock while
         //we can still satisfy the weigh constraint. This is equivilant to finding all subsets, but with the
         //constraint that we stop if we cannot add any other clock.
-        return knapsackProb(clocks, 0, weightConstraint);
+        HashMap<String, Integer> memo = new HashMap<>();
+        return knapsackProb(clocks, 0, weightConstraint, memo);
     }
 
 
-    public static int knapsackProb(List<Clock> clocks, int index, int weightLeft) {
+    public static int knapsackProb(List<Clock> clocks, int index, int weightLeft, HashMap<String, Integer> memo) {
         if (weightLeft <= 0) { return 0; }
         if (index >= clocks.size()) { return 0; }
+
+        String k = "" + index + "-" + weightLeft;
+
+        if (memo.containsKey(k)) { return memo.get(k); }
 
         Clock currClock = clocks.get(index);
         int currMaxVal = 0;
         //including current clock (if we can)
         if (weightLeft - currClock.weight >= 0) {
-            currMaxVal = knapsackProb(clocks, index + 1, weightLeft - currClock.weight) + currClock.value;
+            currMaxVal = knapsackProb(clocks, index + 1, weightLeft - currClock.weight, memo) + currClock.value;
         }
 
         //or excluding current clock (we do not reduce weight)
-        currMaxVal = Math.max(currMaxVal, knapsackProb(clocks, index + 1, weightLeft));
+        currMaxVal = Math.max(currMaxVal, knapsackProb(clocks, index + 1, weightLeft, memo));
+        memo.put(k, currMaxVal);
+        System.out.println("SIZE OF MAP: " + memo.size());
         return currMaxVal;
+
+        //we can easily memo using the arguments, if we are at a particular index and the weightLeft is the same,
+        //we should know what the max is. Therefore, we can use a hashtable, where our key is the index-weightleft
+        //and value is the maximum value we can hold.
+
+        //This reduces our running time complexity to O(num of subproblems * RT of each). RT of each sub problem is
+        //O(1)-- it is simple arithmetic. the number of subproblems is the different combinations of index and weight-left,
+        //which is dependent on clocks. At most, all clocks can fit in the knapsack, giving this a running time of
+        // O(numClocks * Weight)
     }
 
 
