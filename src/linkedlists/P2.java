57cf931685cc2e7acc6a0fe7ae955ddb19bd38d3
alexisherrera <halexis@sas.upenn.edu>
2017-10-02 16:04:58 -0400

    working on ll

--- src/linkedlists/P2.java
+++ src/linkedlists/P2.java
@@ -4,20 +4,12 @@ package linkedlists;
  * Created by alexisherrera on 9/9/17.
  */
 public class P2 {
-    //reverse a linked list sublist from index i to index j.
+    //reverse a linked list sublist from index i to index j. numbering of the nodes begins at one
 
     public static void reverseSublist(ListNode<Integer> list, int i, int j) {
-        int distance = j - i;
 
-        //iterate to start
-        for (int k = 1; k < i; k++) {
-            list = list.next;
-        }
 
-        //list is the element at i
-        //we are going to keep track have 3 nodes in which the first will be the left most part of the list.
-        //the second will be the element we are trying to reverse. and the third will be right most part of the list;
-        //we will point the second node to the first part. thwn update every element by one
+
 
     }
 }
---
+++ src/searching/P4.java
@@ -0,0 +1,8 @@
+package searching;
+
+/**
+ * Created by alexisherrera on 9/27/17.
+ */
+public class P4 {
+    //compute the integer square root
+}
