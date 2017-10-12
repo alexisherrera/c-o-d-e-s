6928d024953b3d1c0d5efb2b15472c21b9f5a5cc
alexisherrera <halexis@sas.upenn.edu>
2017-10-11 22:40:56 -0400

    better RT for triplet

--- src/leetcode/IncreasingTripletSubsequence.java
+++ src/leetcode/IncreasingTripletSubsequence.java
@@ -4,6 +4,32 @@ package leetcode;
  * Created by alexisherrera on 10/11/17.
  */
 public class IncreasingTripletSubsequence {
+
+    public boolean betterComplexity(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int secMin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            //new smallest element
+            if (min >= nums[i]) {
+                min = nums[i];
+            }
+            //new secMin
+            else if (secMin > nums[i]) {
+                secMin = nums[i];
+            }
+            //we found the thirdMaxValue
+            else if (nums[i] > secMin) {
+                return true;
+            }
+
+        }
+
+        return false;
+        //just find smallest element then a larger element and a larger.
+    }
+
+
     public boolean increasingTriplet(int[] nums) {
         return helper(nums, 0, -1, 0);
     }
