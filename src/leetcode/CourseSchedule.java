76407285cfdb8249c7750894777c3d8cef07537e
alexisherrera <halexis@sas.upenn.edu>
2017-10-13 11:06:53 -0400

    course schedule

---
+++ src/leetcode/CourseSchedule.java
@@ -0,0 +1,55 @@
+package leetcode;
+import java.util.*;
+/**
+ * Created by alexisherrera on 10/13/17.
+ */
+public class CourseSchedule {
+    //essentially conduct DFS on the graph and check for cycles
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //first step is to create the graph
+        Map<Integer, List<Integer>> g = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> globalV = new HashSet<>();
+
+        //add skeletal code
+        for (int i = 0; i < numCourses; i++) {
+            g.put(i, new ArrayList<Integer>());
+        }
+
+        //add the edges provided
+        for (int[] edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+
+            List<Integer> edgeList = g.get(from);
+            edgeList.add(to);
+            g.put(from, edgeList);
+        }
+
+        //do DFS on each node
+        for (int i = 0; i < numCourses; i++) {
+            if (globalV.contains(i)) { continue; }
+            if (!DFS(g, i, visited, globalV)) { return false; }
+        }
+
+        return true;
+    }
+
+    public boolean DFS(Map<Integer, List<Integer>> g, int node, Set<Integer> visited, Set<Integer> globalV) {
+        if (visited.contains(node)) { return false; }
+        visited.add(node);
+        globalV.add(node);
+
+        //search adjacent edges
+        for (Integer e : g.get(node)) {
+            if (!DFS(g, e, visited, globalV)) {
+                return false;
+            }
+        }
+
+        //finished visiting all adjacent nodes so unmark current node as visited
+        visited.remove(node);
+        return true;
+    }
+}
--- src/searching/P4.java
+++ src/searching/P4.java
@@ -5,4 +5,49 @@ package searching;
  */
 public class P4 {
     //compute the integer square root
+
+    //approach we can start at 1 and then keep squaring the value until it is over our target.
+    public int bruteforce(int N) {
+        int potentialCandidate = 1;
+        for (int i = 2; i < N; i++) {
+            if (i * i <= N) {
+                potentialCandidate = i;
+            }
+            else if ( i * i > N) { break; }
+        }
+        //takes O(n) time.
+        return potentialCandidate;
+    }
+
+    //how can we improve? We can try the element at N/2 then work to the left and right
+    //depending on the value. Kinda like binary search
+
+    public int BS(int N) {
+        //will do iterative BS
+
+        int lo = 0;
+        int hi = N;
+        int candidate = 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (mid * mid <= N) {
+                candidate = mid;
+                lo = mid + 1;
+            }
+            else {
+                hi = mid -1;
+            }
+        }
+
+        return candidate;
+
+
+
+
+
+
+
+    }
 }
