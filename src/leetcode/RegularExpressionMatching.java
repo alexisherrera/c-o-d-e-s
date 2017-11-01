3621d281b574546ecf573db66722cbefd30f40f7
alexisherrera <halexis@sas.upenn.edu>
2017-11-01 19:21:21 -0400

    regex matching

---
+++ src/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * Created by alexisherrera on 11/1/17.
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+
+        //base case
+        if (p.length() == 0) { return s.length() == 0; }
+
+        //general case
+
+        //are first characters the same
+        boolean sameFirst = s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+
+        //check for a star
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            //check that first characters match and continue using the same front character.
+            //also check if we can match by skipping wildcard and all characters before
+            return (sameFirst && isMatch(s.substring(1), p) ||
+                    isMatch(s, p.substring(2)));
+        }
+        else if (sameFirst) {
+            return isMatch(s.substring(1), p.substring(1));
+        }
+        return false;
+    }
+}
