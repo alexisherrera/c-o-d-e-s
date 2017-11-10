f9eb8faa45ba00024ef12253fceced7ee0bec2e0
alexisherrera <halexis@sas.upenn.edu>
2017-11-10 14:42:35 -0500

    RT analysis

--- src/leetcode/WordLadder.java
+++ src/leetcode/WordLadder.java
@@ -45,5 +45,6 @@ public class WordLadder {
         }
         return 0;
         //Running time is n words, and lets say they are s long. we at most iterate through each word O(s) times. Running time is O(n*s)
+        //RT
     }
 }
